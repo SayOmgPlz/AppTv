9e0724650d0d7fc1e3d6d3c0b8f4a26f8b157726
L <l@78-83-110-3.spectrumnet.bg>
2014-03-22 21:44:27 +0200

    Working radio city

---
+++ src/com/lytcho/apptv/MainActivity.java
@@ -0,0 +1,83 @@
+package com.lytcho.apptv;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback, OnPreparedListener{
+ 
+ MediaPlayer mediaPlayer;
+ SurfaceHolder surfaceHolder;
+ SurfaceView playerSurfaceView;
+ String videoSrc = "http://62.204.145.218:8000/city64";
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+  playerSurfaceView = (SurfaceView)findViewById(R.id.playersurface);
+
+        surfaceHolder = playerSurfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+ }
+
+ @Override
+ public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+  // TODO Auto-generated method stub1
+  
+ }
+
+ @Override
+ public void surfaceCreated(SurfaceHolder arg0) {
+
+        try {
+         mediaPlayer = new MediaPlayer();
+    mediaPlayer.setDisplay(surfaceHolder);
+   mediaPlayer.setDataSource(videoSrc);
+   mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+   mediaPlayer.prepare();
+   mediaPlayer.setOnPreparedListener(this);
+  
+  } catch (IllegalArgumentException e) {
+	  String message = e.getMessage();
+	  Log.i("TAG", ((message == null) ? "string null" : message));
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  } catch (SecurityException e) {
+	  String message = e.getMessage();
+	  Log.i("TAG", ((message == null) ? "string null" : message));
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  } catch (IllegalStateException e) {
+	  String message = e.getMessage();
+	  Log.i("TAG", ((message == null) ? "string null" : message));
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  } catch (IOException e) {
+	  String message = e.getMessage();
+	  Log.i("TAG", ((message == null) ? "string null" : message));
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+ }
+
+ @Override
+ public void surfaceDestroyed(SurfaceHolder arg0) {
+  // TODO Auto-generated method stub
+  
+ }
+
+ @Override
+ public void onPrepared(MediaPlayer mp) {
+  mediaPlayer.start();
+ }
+
+}
+
