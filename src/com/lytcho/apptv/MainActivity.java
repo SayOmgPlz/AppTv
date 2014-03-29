6949dfc500c42423d8eccef2064c35040e48c5e7
Lutchezar Canov <lecanov@abv.bg>
2014-03-29 15:50:59 +0200

    Imported View library

--- src/com/lytcho/apptv/MainActivity.java
+++ src/com/lytcho/apptv/MainActivity.java
@@ -2,6 +2,7 @@ package com.lytcho.apptv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.MediaController;
