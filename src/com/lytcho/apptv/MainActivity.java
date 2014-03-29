2df94d8a19b10f5eec778735fedb03f1932b870d
--global <--global>
2014-03-29 15:10:27 +0200

    1. The main Application view ver.1, 2. Deleted the Channels Activity

--- src/com/lytcho/apptv/MainActivity.java
+++ src/com/lytcho/apptv/MainActivity.java
@@ -1,42 +1,47 @@
 package com.lytcho.apptv;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity {
- String videoSrc = "http://m24.megafun.vn/live.vs?c=vstv042&q=high";
+ String videoSrc = "http://hd3.lsops.net/live/smil:aljazeer_ar_hls/playlist.m3u8";
 
- @Override
- protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main); 
+@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// TODO: ASYNC FILL WITH API CALL
+		fillChannelList();
+		
+		startVideView();
+	}
+ 
+	private void fillChannelList() {
+		String[] channels = {"teveliziq 1", "televiziq 2", "aljazira"};
+		ListView listOfChannels = (ListView)findViewById(R.id.listOfChannels);
+		ArrayAdapter<String> arrayOfChannelsAdapter = new ArrayAdapter<String>(getBaseContext(),
+				  R.layout.channel_item,
+				  channels);
+		listOfChannels.setAdapter(arrayOfChannelsAdapter);
+		 
+	}
 	
-	final VideoView videoView = (VideoView) 
-                      findViewById(R.id.videoView1);
-	
+	private void startVideView() {
+		final VideoView videoView = (VideoView) 
+                findViewById(R.id.focusedTv);
 
-	videoView.setVideoPath(
-			videoSrc);
-	
-	Button openChannelsButton = (Button)findViewById(R.id.button1);
-	openChannelsButton.setOnClickListener(new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Intent intent = new Intent(getApplicationContext(), Channels.class);
-			startActivity(intent);
-		}
-	});
-	
-	MediaController mediaController = new MediaController(this);
-	mediaController.setAnchorView(videoView);
-	videoView.setMediaController(mediaController);
-	
-	videoView.start();		
- }
+
+		videoView.setVideoPath(
+				videoSrc);
+		
+		MediaController mediaController = new MediaController(this);
+		mediaController.setAnchorView(videoView);
+		videoView.setMediaController(mediaController);
+		
+		videoView.start();		
+	}
 }
